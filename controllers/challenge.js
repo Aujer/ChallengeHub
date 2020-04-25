3399c4ac7ad14d78ac704ae5ab09d93681dbaa88
Alex Yang <alex.yang@berkeley.edu>
2020-04-25 16:51:37 -0700

    updated challenge controller

--- controllers/challenge.js
+++ controllers/challenge.js
@@ -91,3 +91,68 @@ exports.postContact = (req, res) => {
       return res.redirect('/contact');
     });
 };
+
+// Alex's part
+
+const validator = require('validator');
+const nodemailer = require('nodemailer');
+const Challenge = require('../models/Challenge');
+const User = require('../models/User');
+
+
+/**
+ * GET /new-challenge
+ * New challenge page.
+ */
+exports.getChallenge = (req, res) => {
+  if (!req.user) {
+    return res.redirect('/login');
+  }
+  res.render('challenge', {
+    title: 'Start a new challenge!'
+  });
+};
+
+/**
+ * POST /challenge-accepted
+ * Post a new challenge.
+ */
+exports.postChallenge = (req, res, next) => {
+  const validationErrors = [];
+  // if some errors, add to validationErrors
+  if (validationErrors.length) {
+    req.flash('errors', validationErrors);
+    return res.redirect('/new-challenge');
+  }
+  var challenge = new Challenge({
+    challenge_name: req.body.challenge_name,
+    description: req.body.description,
+    creator: req.currentUser,
+  });
+  var db = req.db;
+  db.collection('New Challenges').insertOne(challenge,function(err) {
+    if (err) throw err;
+    console.log("Challenge uploaded successfully!")
+  })
+  // want to create a new web page with this as the challenge and redirect to that web page
+
+  // for now create a temporary rendering
+  req.flash('success', { msg: 'Challenged uploaded!' });
+  res.render('challenge', {
+    title: 'Challenge uploaded!'
+  })
+  //return res.redirect('/challenge-submitted');
+};
+
+/**
+ * GET /challenge-submitted
+ * Submitted challenge page.
+ */
+exports.getSubmittedChallenge = (req, res) => {
+  if (!req.user) {
+    return res.redirect('/login');
+  }
+  res.render('tempChallenge', {
+    title: 'The challenge begins!'
+  });
+};
