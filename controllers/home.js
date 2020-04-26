4198a9b6a7ba907dec6b5fa8addd5895ec908f46
aujer <aujerding@gmail.com>
2020-04-26 07:07:45 -0400

    aesthetics

--- controllers/home.js
+++ controllers/home.js
@@ -19,35 +19,67 @@ else {
   var query = { "creator": 0 };
 }
 console.log(query)
-	db.collection("subscriptions").find({ "user": ObjectId(req.user._id)}).toArray(function(err3, result3) {
-    db.collection("New Challenges").find().toArray(function(err1, result1) {
-      if (err1) throw err1;
-        db.collection("New Challenges").find(query).toArray(function(err, result) {
-          if (err) throw err;
-            db.collection("Challenge Uploads").find().toArray(function(err2, result2) {
-              if (err2) throw err2;
-              console.log(result);
-              if (req.user) {
-                  res.render('home', {
-                title: 'Home',
-                city: "tokyo",
-                list_of_challenges: result,
-                popular_challenges: result1.splice(0,6),
-                subscribed_challenges: result3,
-                recent_uploads: result2
-              });
-              }
-              else {
-                res.render("home_logged_out", {
-                  title: "Home_logged_out",
-                  popular_challenges: result1.slice(0,6),
-                  recent_uploads: result2
-                })
-              }
-          });
-            });
-          })
+	if (req.user) {
+				db.collection("subscriptions").find({ "user": ObjectId(req.user._id)}).toArray(function(err3, result3) {
+			    db.collection("New Challenges").find().toArray(function(err1, result1) {
+			      if (err1) throw err1;
+			        db.collection("New Challenges").find(query).toArray(function(err, result) {
+			          if (err) throw err;
+			            db.collection("Challenge Uploads").find().toArray(function(err2, result2) {
+			              if (err2) throw err2;
+			              console.log(result);
+			              if (req.user) {
+			                  res.render('home', {
+			                title: 'Home',
+			                city: "tokyo",
+			                list_of_challenges: result,
+			                popular_challenges: result1.splice(0,6),
+			                subscribed_challenges: result3,
+			                recent_uploads: result2
+			              });
+			              }
+			              else {
+			                res.render("home_logged_out", {
+			                  title: "Home_logged_out",
+			                  popular_challenges: result1.slice(0,6),
+			                  recent_uploads: result2
+			                });
+			              };
+			         	 });
+			            });
+			          });
 
-        })
+			        });
+	}
+	else {
+		db.collection("New Challenges").find().toArray(function(err1, result1) {
+			      if (err1) throw err1;
+			        db.collection("New Challenges").find(query).toArray(function(err, result) {
+			          if (err) throw err;
+			            db.collection("Challenge Uploads").find().toArray(function(err2, result2) {
+			              if (err2) throw err2;
+			              console.log(result);
+			              if (req.user) {
+			                  res.render('home', {
+			                title: 'Home',
+			                city: "tokyo",
+			                list_of_challenges: result,
+			                popular_challenges: result1.splice(0,6),
+			                subscribed_challenges: result3,
+			                recent_uploads: result2
+			              });
+			              }
+			              else {
+			                res.render("home_logged_out", {
+			                  title: "Home_logged_out",
+			                  popular_challenges: result1.slice(0,6),
+			                  recent_uploads: result2
+			                });
+			              };
+			         	 });
+			            });
+			          });
+	}
+
 
         };
