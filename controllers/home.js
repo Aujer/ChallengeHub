adfd006dbf60835a0ec586d25f0616062aa8a13a
aujer <aujerding@gmail.com>
2020-04-25 23:28:39 -0400

    home page: challenges you're hosting

--- controllers/home.js
+++ controllers/home.js
@@ -3,9 +3,28 @@
  * Home page.
  */
 exports.index = (req, res) => {
-  res.render('home', {
-    title: 'Home'
-  });
+
+var db=req.db
+var ObjectId = require('mongodb').ObjectId; 
+
+console.log("dingo")
+console.log(req.user)
+console.log("tammy")
+
+var query = { "creator": ObjectId(req.user._id) };
+console.log(query)
+        db.collection("New Challenges").find(query).toArray(function(err, result) {
+          if (err) throw err;
+          console.log(result);
+          res.render('home', {
+            title: 'Home',
+            city: "tokyo",
+            list_of_challenges: result,
+          });
+          
+    })
+
+
 };
 
 
